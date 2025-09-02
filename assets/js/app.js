3a797555dc47f05cb577bc9ae903745e4dce1804
gajanankdm <gajanankadam0965@gmail.com>
2025-09-02 20:47:30 +0530

    work on js

---
+++ assets/js/app.js
@@ -0,0 +1,42 @@
+const cl = console.log;
+
+const setRating = (rating) =>{
+	
+	if(rating >= 7){
+		return "badge-success";
+	}else if(rating >= 5 && rating < 7){
+		return "badge-warning";
+	}else{
+		return "badge-danger";
+	}
+};
+
+let result = "";
+moviesArry.forEach(movie =>{
+	result += `<div class="col-lg-4 col-md-6 col-sm-12 mb-4">
+	    <div class="card">
+		 <figure class="MovieCard mb-0">
+		  <img src= ${movie.poster_path}>
+		  <figcaption>
+		   <div class="titleInfo">
+		    <div class="row">
+			 <div class="col-10">
+			  <h2>${movie.original_title}</h2>
+			 </div>
+			  <div class="col-2">
+			   <span class="badge ${setRating(movie.vote_average)}">${movie.vote_average}</span>
+			  </div>
+			</div>
+			</div>
+			<div class="overview">
+			 <h5>${movie.title}</h5>
+			 <p>${movie.overview}</p>
+			</div>
+		  </figcaption>
+		 </figure>
+		</div>
+	   </div>
+	  </div> `
+});
+
+ document.getElementById("postContainer").innerHTML=result;
